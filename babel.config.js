8f00f786d3f0b35c0bfc7d33abbf0c16e4c5e36d
Lee Rudd <leerudd3@gmail.com>
2023-12-22 10:22:39 -0800

    attempted fix at running test

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,33 @@
-module.exports = {
-  "presets": [
+/* eslint-disable func-names */
+module.exports = function (api) {
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ];
+  const plugins = [
     [
-      "@babel/preset-env", {
-        "targets": {
-          "node": "current"
-        }
-      },
-      "@babel/preset-react"
-    ]
-  ],
-  "plugins": [
-    [
-      "@babel/plugin-transform-runtime", {
-        "regenerator": true
+      '@babel/plugin-transform-spread',
+      {
+        loose: false
       }
     ],
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-export-default-from",
-    ["@babel/plugin-transform-react-jsx", { "pragma":"h" }]
-  ]
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: false
+      }
+    ],
+    '@babel/plugin-transform-runtime'
+  ];
+
+  /** this is just for minimal working purposes,
+     * for testing larger applications it is
+     * advisable to cache the transpiled modules in
+     * node_modules/.bin/.cache/@babel/register* */
+  api.cache(false);
+
+  return {
+    presets,
+    plugins
+  };
 };
